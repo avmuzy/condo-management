5b1ee79020b69d2b9266c2a8ccd470e9a7d929e5
avms <andrevmuzy@gmail.com>
2022-07-10 16:53:02 -0300

    condo class

--- condominium.py
+++ condominium.py
@@ -2,6 +2,10 @@ class Condominium:
     def __init__(self):
         self.street_address = 'Waldo Avenue'
         self.number = 3668
+        self.suite = 501
+        self.month_fee = 350.00
+        self.month_tax = 55.00
+
 
 
 Condominium1 = Condominium()
