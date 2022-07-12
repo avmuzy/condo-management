4e5327815cde412afb3bdad0c5d1997b7402bfaf
avms <andrevmuzy@gmail.com>
2022-07-12 20:08:50 -0300

    condo class

--- condo2.py
+++ condo2.py
@@ -3,3 +3,17 @@ class Condominium:
         print('Condominium created')
         self.street_address = 'Waldo Avenue'
         self.number = 3668
+
+    def identify(self):
+        print('Condominium')
+
+class Apartment(Condominium):
+    def __init__(self):
+        Condominium.__init__(self)
+        print('Apartment created')
+
+    def identify(self):
+        print('Apartment')
+
+
+suite401 = Apartment()
