ee15db3e3387e207ffb0fdbf74fe4c33c40642bc
avms <andrevmuzy@gmail.com>
2022-07-13 19:21:48 -0300

    condo class

--- condo2.py
+++ condo2.py
@@ -7,13 +7,19 @@ class Condominium:
     def identify(self):
         print('Condominium')
 
+
 class Apartment(Condominium):
-    def __init__(self):
+    def __init__(self, suite, month_fee, month_tax, kind, area):
         Condominium.__init__(self)
         print('Apartment created')
+        self.suite = suite
+        self.month_fee = month_fee
+        self.month_tax = month_tax
+        self.kind = kind
+        self.area = area
 
     def identify(self):
         print('Apartment')
 
 
-suite401 = Apartment()
+suite401 = Apartment(401, 350, 50, 'Apartment', 44)
