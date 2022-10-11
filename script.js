2852de5bfe04e868ede2c0b3e26549612f447223
avms <andrevmuzy@gmail.com>
2022-10-11 19:22:33 -0300

    calculate

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 function calc() {
-  let apto = document.getElementById('condapto');
-  let aptoNum = Number(apto.value);
-  let txextra = document.getElementById('txextra');
-  let txNum = Number(txextra.value);
+  let apto201 = document.getElementById('201');
+  let aptoNum = Number(apto201.value);
+  let tx201 = document.getElementById('tx201');
+  let txNum = Number(tx201.value);
   let total = aptoNum + txNum;
-  let res = document.getElementById('res');
-  res.innerHTML = `${total}`;
+  let res201 = document.getElementById('res201');
+  res201.innerHTML = `${total}`;
+
+  let apto202 = document.getElementById('202');
+  let aptoNum202 = Number(apto202.value);
+  let tx202 = document.getElementById('tx202');
+  let txNum202 = Number(tx202.value);
+  let total202 = aptoNum202 + txNum202;
+  let res202 = document.getElementById('res202');
+  res202.innerHTML = `${total202}`;
+
+  let apto301 = document.getElementById('301');
+  let aptoNum301 = Number(apto301.value);
+  let tx301 = document.getElementById('tx301');
+  let txNum301 = Number(tx301.value);
+  let total301 = aptoNum301 + txNum301;
+  let res301 = document.getElementById('res301');
+  res301.innerHTML = `${total301}`;
+
+  let apto302 = document.getElementById('301');
+  let aptoNum302 = Number(apto302.value);
+  let tx302 = document.getElementById('tx302');
+  let txNum302 = Number(tx302.value);
+  let total302 = aptoNum302 + txNum302;
+  let res302 = document.getElementById('res302');
+  res302.innerHTML = `${total302}`;
+
+  let apto401 = document.getElementById('401');
+  let aptoNum401 = Number(apto401.value);
+  let tx401 = document.getElementById('tx401');
+  let txNum401 = Number(tx401.value);
+  let total401 = aptoNum401 + txNum401;
+  let res401 = document.getElementById('res401');
+  res401.innerHTML = `${total401}`;
+
+  let apto402 = document.getElementById('402');
+  let aptoNum402 = Number(apto402.value);
+  let tx402 = document.getElementById('tx402');
+  let txNum402 = Number(tx402.value);
+  let total402 = aptoNum402 + txNum402;
+  let res402 = document.getElementById('res402');
+  res402.innerHTML = `${total402}`;
+
+  let apto501 = document.getElementById('501');
+  let aptoNum501 = Number(apto501.value);
+  let tx501 = document.getElementById('tx501');
+  let txNum501 = Number(tx501.value);
+  let total501 = aptoNum501 + txNum501;
+  let res501 = document.getElementById('res501');
+  res501.innerHTML = `${total501}`;
 }
